9f3dc6005ed4ba80e1ff3b12743e7f041572bc26
Racle <43697210+RacleRay@users.noreply.github.com>
2020-08-05 23:32:45 +0800

    add DDPG

---
+++ Quadrotor_DDPG/model.py
@@ -0,0 +1,110 @@
+
+import os
+import numpy as np
+import parl
+from parl import layers
+from paddle import fluid
+from parl.utils import logger
+from parl.utils import action_mapping # 将神经网络输出映射到对应的 实际动作取值范围 内
+from parl.utils import ReplayMemory # 经验回放
+from parl.algorithms import DDPG
+
+
+"""
+Agent把产生的数据传给algorithm，algorithm根据model的模型结构计算出Loss，使用SGD或者其他优化器不断的优化
+"""
+
+
+class ActorModel(parl.Model):
+    def __init__(self, act_dim):
+        hid_size = 100
+        self.fc1 = layers.fc(size=hid_size, act='relu')
+        self.fc2 = layers.fc(size=act_dim, act='tanh')
+
+    def policy(self, obs):
+        hid = self.fc1(obs)
+        logits = self.fc2(hid)
+        return logits
+
+
+class CriticModel(parl.Model):
+    def __init__(self):
+        hid_size = 100
+        self.fc1 = layers.fc(size=hid_size, act='relu')
+        self.fc2 = layers.fc(size=1, act=None)
+
+    def value(self, obs, act):
+        # 输入 state, action, 输出对应的Q(s,a)
+        concat = layers.concat([obs, act], axis=1)
+        hid = self.fc1(concat)
+        Q = self.fc2(hid)
+        Q = layers.squeeze(Q, axes=[1])
+        return Q
+
+
+class QuadrotorModel(parl.Model):
+    def __init__(self, act_dim):
+        self.actor_model = ActorModel(act_dim)
+        self.critic_model = CriticModel()
+
+    def policy(self, obs):
+        return self.actor_model.policy(obs)
+
+    def value(self, obs, act):
+        return self.critic_model.value(obs, act)
+
+    def get_actor_params(self):
+        return self.actor_model.parameters()
+
+
+class QuadrotorAgent(parl.Agent):
+    def __init__(self, algorithm, obs_dim, act_dim=4):
+        assert isinstance(obs_dim, int)
+        assert isinstance(act_dim, int)
+        self.obs_dim = obs_dim
+        self.act_dim = act_dim
+        super(QuadrotorAgent, self).__init__(algorithm)
+
+        # 注意，在最开始的时候，先完全同步target_model和model的参数
+        self.alg.sync_target(decay=0)
+
+    def build_program(self):
+        self.pred_program = fluid.Program()
+        self.learn_program = fluid.Program()
+
+        with fluid.program_guard(self.pred_program):
+            obs = layers.data(
+                name='obs', shape=[self.obs_dim], dtype='float32')
+            self.pred_act = self.alg.predict(obs)
+
+        with fluid.program_guard(self.learn_program):
+            obs = layers.data(
+                name='obs', shape=[self.obs_dim], dtype='float32')
+            act = layers.data(
+                name='act', shape=[self.act_dim], dtype='float32')
+            reward = layers.data(name='reward', shape=[], dtype='float32')
+            next_obs = layers.data(
+                name='next_obs', shape=[self.obs_dim], dtype='float32')
+            terminal = layers.data(name='terminal', shape=[], dtype='bool')
+            _, self.critic_cost = self.alg.learn(obs, act, reward, next_obs,
+                                                 terminal)
+
+    def predict(self, obs):
+        obs = np.expand_dims(obs, axis=0)
+        act = self.fluid_executor.run(
+            self.pred_program, feed={'obs': obs},
+            fetch_list=[self.pred_act])[0]
+        return act
+
+    def learn(self, obs, act, reward, next_obs, terminal):
+        feed = {
+            'obs': obs,
+            'act': act,
+            'reward': reward,
+            'next_obs': next_obs,
+            'terminal': terminal
+        }
+        critic_cost = self.fluid_executor.run(
+            self.learn_program, feed=feed, fetch_list=[self.critic_cost])[0]
+        self.alg.sync_target()
+        return critic_cost
